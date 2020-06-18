33fbd3bb950ca1109e4702f395e66de4bc9d6114
Sae Woo Nam <nams@nist.gov>
2020-06-18 17:56:33 -0600

    fix but introduced by changing menu_info to m_info.py

--- m_write_start.py
+++ m_write_start.py
@@ -1,5 +1,5 @@
 import Adafruit_BluefruitLE
-import menu_info
+import m_info
 import sys
 
 if __name__ == '__main__':
@@ -14,13 +14,13 @@ if __name__ == '__main__':
 
     # Get the BLE provider for the current platform.
     ble = Adafruit_BluefruitLE.get_provider()
-    menu_info.ble = ble
-    menu_info.g_number_to_find = g_number_to_find
-    menu_info.g_command = g_command
+    m_info.ble = ble
+    m_info.g_number_to_find = g_number_to_find
+    m_info.g_command = g_command
     # Initialize the BLE system.  MUST be called before other BLE calls!
     ble.initialize()
 
     # Start the mainloop to process BLE events, and run the provided function in
     # a background thread.  When the provided main function stops running, returns
     # an integer status code, or throws an error the program will exit.
-    ble.run_mainloop_with(menu_info.main)
+    ble.run_mainloop_with(m_info.main)
